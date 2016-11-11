1e2f0a66e90449f0d4507bd37d217d5c36a434f9
lazaro <lazaro@lazaro-K52F>
2016-11-10 22:35:57 -0300

    Clase para transformar string a sql.date

---
+++ src/util/Parse.java
@@ -0,0 +1,24 @@
+package util;
+
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+public class Parse {
+
+	public static Date dateToSql(String fecha) throws Exception{
+		java.sql.Date sqlDate = null;
+		try{
+			SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+			java.util.Date date = format.parse(fecha);
+			sqlDate = new java.sql.Date(date.getTime());
+		}
+		catch(ParseException pex){
+			throw new AppException("Error al intentar convertir las fechas");
+		}
+		catch(Exception ex){
+			throw new Exception("Error no controlado al intentar convertir las fechas");
+		}
+		return sqlDate;	
+	}
+}
